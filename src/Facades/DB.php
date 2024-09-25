db1479163b6334b572620b7a60dd20cc2c299b01
jundayw <jundayw@126.com>
2024-09-25 13:38:15 +0800

    DB

---
+++ src/Facades/DB.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Nacosvel\DatabaseManager\Facades;
+
+use Nacosvel\Contracts\DatabaseManager\DatabaseManagerInterface;
+use Nacosvel\DatabaseManager\DatabaseManager;
+use Nacosvel\Facades\Facade;
+use RuntimeException;
+use function Nacosvel\Container\Interop\application;
+
+/**
+ * @see DatabaseManager
+ */
+class DB extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     *
+     * @throws RuntimeException
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        if (false === application()->getContainer()->has(DatabaseManagerInterface::class)) {
+            application()->bind(DatabaseManagerInterface::class, function () {
+                if (application()->getContainer()->has('db')) {
+                    return new DatabaseManager(application('db'));
+                }
+                throw new RuntimeException(sprintf('Please bind the instance of %s to the container.', DatabaseManagerInterface::class));
+            });
+        }
+        return DatabaseManagerInterface::class;
+    }
+
+    /**
+     * Get a resolved facade instance.
+     *
+     * @return DatabaseManagerInterface
+     */
+    protected static function getFacadeInstance(): DatabaseManagerInterface
+    {
+        return application(DatabaseManagerInterface::class);
+    }
+
+}
