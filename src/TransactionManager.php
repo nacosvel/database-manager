dbcbfa9891b4c4abce28c16327adf9692e517d6f
jundayw <jundayw@126.com>
2024-11-02 17:19:57 +0800

    docs

--- src/TransactionManager.php
+++ src/TransactionManager.php
@@ -3,7 +3,24 @@
 namespace Nacosvel\DatabaseManager;
 
 use Nacosvel\Contracts\DatabaseManager\DatabaseManagerInterface;
+use PDO;
 
+/**
+ * Class TransactionManager
+ *
+ * @method null|string getName() Get the database connection name.
+ * @method mixed getConnectionConfiguration(string $option = null, mixed $default = null) Get the configuration for a connection.
+ * @method PDO getPdo() Get the current PDO connection.
+ * @method void unprepared(string $query) Run a raw, unprepared query against the PDO connection.
+ * @method void beginTransaction() Start a new database transaction.
+ * @method void commit() Commit the active database transaction.
+ * @method void rollBack() Rollback the active database transaction.
+ * @method void xaBeginTransaction(string $xid) Starts a distributed XA transaction with the given XID.
+ * @method void xaPrepare(string $xid) Prepares the distributed XA transaction with the given XID for commit.
+ * @method void xaCommit(string $xid) Commits the distributed XA transaction with the given XID.
+ * @method void xaRollBack(string $xid) Rolls back the distributed XA transaction with the given XID.
+ * @method void xaRecover(string $xid) Recovers the distributed XA transaction with the given XID.
+ */
 class TransactionManager implements DatabaseManagerInterface
 {
     public function __construct(
